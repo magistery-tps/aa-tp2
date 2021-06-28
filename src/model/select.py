98dd8b215bf519dc5e490f0830329fd0745c943a
adrianmarino <adrianmarino@gmail.com>
2021-06-27 22:19:41 -0300

    feat: tuning

--- src/model/select.py
+++ src/model/select.py
@@ -3,6 +3,7 @@ def get_max_score_model(models, X_test, y_test):
 
     for model in models:
         score = model.score(X_test, y_test)
+        print('- {}: {:.4f}'.format(type(model).__name__, score))
         if score > max_score:
             max_score, best_model = score, model
 
