53ec28fe7fa12393b2eb345993286f53b795aac4
corpa00114 <adrianmarino@gmail.com>
2021-07-24 13:00:07 -0300

    feat: add snr cumpute correction

--- src/sound/noise.py
+++ src/sound/noise.py
@@ -11,13 +11,41 @@ def create_dir(path):
     return path
             
 def snr(audio, noise):
-    return 20 * np.log10(np.quantile(rms(audio), 0.9) / np.quantile(rms(0.005 * noise), 0.9))
+    """
+        Get signal-to-noise ratio.
 
-def append_noise(audio, level=1):
-    noise = np.random.uniform(low=-level, high=level, size=(len(audio),))
-    return snr(audio, noise) * noise + audio
+        SRN = quantile 0.9 of audio energy / quantile 0.9 of noise energy
+        
+        Notes:
+        - rms: Is the audio energy.
+        - quantile(rms, 0.9): Compute rms 0.9 quantile. This allows as exclude audio or noise silence signals. 
+        
+        See: https://youtu.be/aruFMX7f3fk?t=2993
+    """
+    return rms_q_09(audio) / rms_q_09(noise)
 
-def create_new_file_with_noise(path, level):
+
+def rms_q_09(audio): return np.quantile(rms(audio), 0.9)
+
+def append_noise(original_audio, snr_ratio, level=0.001):
+    '''
+        Append noise base on oiginal audio SNR ratio.
+    '''
+    noise = np.random.uniform(
+        low=-level, 
+        high=level, 
+        size=(len(original_audio),)
+    )
+
+    new_snr = snr(original_audio, noise) * snr_ratio
+
+    desired_rms = (rms_q_09(noise) *10) ** (new_snr/20.0)
+
+    noise = (desired_rms / rms_q_09(original_audio)) * noise
+
+    return noise + original_audio
+
+def create_new_file_with_noise(path, snr_ratio=1, level=0.001):
     audio, samplerate = sf.read(path)
     
     file_parts = path.split('/')
@@ -25,5 +53,5 @@ def create_new_file_with_noise(path, level):
     destiny_path = create_dir('/'.join(file_parts[:-1]) + '/noisy')
     destiny_path += '/{}-{}'.format(uuid.uuid1(), file_parts[-1])
 
-    sf.write(destiny_path, append_noise(audio, level), samplerate)
+    sf.write(destiny_path, append_noise(audio, snr_ratio, level), samplerate)
     return destiny_path
